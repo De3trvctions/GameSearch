a7e96189422bcf2e227e85d7e680235137ae905d
Kelden <keldenong@gmail.com>
2019-11-25 23:14:52 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,8 @@ import {
 } from 'reactstrap';
 import Swal from 'sweetalert2';
 
+const URL = "https://game-search-server666.herokuapp.com/";
+
 class App extends Component {
     constructor() {
         super();
@@ -73,7 +75,7 @@ class App extends Component {
         }
         })
         //this.setState({ alertVisible: false });
-        const query = `/create?name=${this.state.title}`;
+        const query = URL + `/create?name=${this.state.title}`;
         axios.get(query)
         .then(result => {
             console.log(result.data);
@@ -110,7 +112,7 @@ class App extends Component {
             this.getAllMovies();
         } 
         else {
-            const query = `/findgame?name=${e.target.value}`;
+            const query = URL + `/findgame?name=${e.target.value}`;
             axios.get(query).then(result => {
                 this.setState({ movies: result.data });
                 console.log(result.data);
@@ -128,7 +130,7 @@ class App extends Component {
     }
 
     getAllMovies = () => {
-        axios.get("/games").then(result => {
+        axios.get(URL + "/games").then(result => {
             this.setState({ movies: result.data });
             console.log(this.state.movies);
         }).catch(error => {
@@ -142,7 +144,7 @@ class App extends Component {
 
     deleteRecord = value => {
         console.log("to delete: ", value);
-        const query = `/delete?id=${value}`;
+        const query = URL +  `/delete?id=${value}`;
         axios.get(query).then(result => {
             this.getAllMovies();
         }).catch(error => {
@@ -155,7 +157,7 @@ class App extends Component {
     };
 
     editRecord = (id, value, genres) => {
-        const query = `/update?id=${id}&text=${value}&genres=${genres}`;
+        const query = URL + `/update?id=${id}&text=${value}&genres=${genres}`;
         console.log(query);
         axios.get(query).then(result => {
             this.getAllMovies();
